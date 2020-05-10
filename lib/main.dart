c21ff61f7a17a1945e7298646859c57f19bee8ac
Mohammed Sahl <sahl.kably@gmail.com>
2020-05-10 16:49:54 -0400

    infinite Listtile!

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Tryna figure out state lol'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'generator',
+      home: RandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asCamelCase);
+  Widget  _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+       if (i.isOdd) return Divider();
+
+        final index = i ~/ 2; 
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(5));
+        }
+        return _buildRow(_suggestions[index]);
+    });
   }
+
+  Widget  _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asCamelCase,
+        style: _biggerFont,
+      )
+    );
+  }
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Startup Name Generator'),
+    ),
+    body: _buildSuggestions(),
+    );
+  }
+  
 }
 
 class RandomWords extends StatefulWidget {
